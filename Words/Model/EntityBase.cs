fff06dbfe532680b3c70ff77ace6396fea78c100
tomi77 <tomislav-ante.brncic@fer.hr>
2017-12-18 14:24:05 +0100

    dodan model layer

---
+++ Words/Model/EntityBase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public abstract class EntityBase<TId> : IEquatable<EntityBase<TId>>
+    {
+        public TId Id { get; set; }
+
+        protected EntityBase(TId id)
+        {
+            if (Equals(id, default(TId)))
+            {
+                throw new ArgumentException("The ID cannot be the default value.", "id");
+            }
+            Id = id;
+        }
+
+        public bool Equals(EntityBase<TId> other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+            return Id.Equals(other.Id);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var entity = obj as EntityBase<TId>;
+            if (entity != null)
+            {
+                return Equals(entity);
+            }
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
---
+++ Words/Model/Word.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Word : EntityBase<int>
+    {
+        public string Text { get; set; }
+        public int BaseWord { get; set; }
+
+        public Word() : base(0)
+        {
+        }
+
+        public Word(int inID, string inText) : base(inID)
+        {
+            Text = inText;
+        }
+    }
+}
