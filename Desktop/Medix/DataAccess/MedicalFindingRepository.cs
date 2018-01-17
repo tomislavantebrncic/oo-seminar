2d305ba17ba6e1979bcb9b5d76cf89dc26d5ff95
Ivana Prebeg <ip48455@fer.hr>
2018-01-17 19:32:47 +0100

    Dodani repozitoriji za MedicalFinding

---
+++ Desktop/Medix/DataAccess/MedicalFindingRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using Model.Repositories;
+using NHibernate;
+
+namespace DataAccess
+{
+    class MedicalFindingRepository : IMedicalFindingRepository
+    {
+        public MedicalFindingRepository()
+        {
+        }
+
+        public void SaveFinding(MedicalFinding finding)
+        {
+            using (ISession nhibernateSession = SessionManager.GetCurrentSession())
+            {
+                nhibernateSession.BeginTransaction();
+                nhibernateSession.SaveOrUpdate(finding);
+                nhibernateSession.Transaction.Commit();
+            }
+        }
+    }
+}
