2f8cf404be91b2d5cf2a0c31b7de7f04e3866677
Ivana Prebeg <ip48455@fer.hr>
2018-01-19 18:13:16 +0100

    dodano mapiranje

---
+++ Desktop/Medix/DataAccess/Mappings/NurseMap.cs
@@ -0,0 +1,24 @@
+﻿using FluentNHibernate.Mapping;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Mappings
+{
+    class NurseMap : ClassMap<Nurse>
+    {
+        public NurseMap()
+        {
+            Id(p => p.Id).GeneratedBy.Native();
+            Map(p => p.FirstName);
+            Map(p => p.LastName);
+            Map(p => p.OIB);
+            Map(p => p.DateOfBirth);
+            Map(p => p.EmployeeID);
+            Map(p => p.Password);
+        }
+    }
+}
