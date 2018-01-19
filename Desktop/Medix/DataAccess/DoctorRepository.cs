cc6895782da3c78ffe17d86330848721573e798d
tomi77 <tomislav-ante.brncic@fer.hr>
2018-01-19 17:10:41 +0100

    Doctor repozitorij update

--- Desktop/Medix/DataAccess/DoctorRepository.cs
+++ Desktop/Medix/DataAccess/DoctorRepository.cs
@@ -15,6 +15,7 @@ namespace DataAccess
         {
             //Doctor d = new Doctor(1, "Tomislav Ante", "Brnčić", "0036486606", new DateTime(), "123456789", "lozinka");
             //return d;
+            /*
             using (ISession nhibernateSession = SessionManager.GetCurrentSession())
             {
                 IQuery query = nhibernateSession.CreateQuery(
@@ -31,6 +32,27 @@ namespace DataAccess
                 else
                 {
                     return foundDoctors[0];
+                }
+            }
+            */
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                    {
+                        IQuery query = session.CreateQuery(
+                            "FROM Doctor");
+
+
+                        IList<Doctor> foundDoctors = query.List<Doctor>();
+
+                        if (foundDoctors.Count == 0)
+                        {
+                            return null;
+                        }
+                        else
+                        {
+                            return foundDoctors[0];
+                        }
                 }
             }
         }
