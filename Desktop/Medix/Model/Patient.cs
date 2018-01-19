3e45c3c1983cbbd5961a80f9f05f2a1560a5426a
tomi77 <tomislav-ante.brncic@fer.hr>
2018-01-19 18:46:20 +0100

    Popravljeno učitavanje pregleda u čekaonu

--- Desktop/Medix/Model/Patient.cs
+++ Desktop/Medix/Model/Patient.cs
@@ -25,5 +25,10 @@ namespace Model
         {
             PatientID = inPatientID;
         }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }
     }
 }
