537b832271f1a976770f061c3a2f96685935fd12
tomi77 <tomislav-ante.brncic@fer.hr>
2018-01-19 18:07:17 +0100

    Prepravljen login

--- Desktop/Medix/DataAccess/DoctorRepository.cs
+++ Desktop/Medix/DataAccess/DoctorRepository.cs
@@ -13,49 +13,27 @@ namespace DataAccess
     {
         public Doctor GetDoctorWithIdAndPassword(string inId, string inPassword)
         {
-            //Doctor d = new Doctor(1, "Tomislav Ante", "Brnčić", "0036486606", new DateTime(), "123456789", "lozinka");
-            //return d;
-            /*
-            using (ISession nhibernateSession = SessionManager.GetCurrentSession())
-            {
-                IQuery query = nhibernateSession.CreateQuery(
-                    "FROM Doctor WHERE employee_id = :username AND password = :password");
-                query.SetString("username", inId);
-                query.SetString("password", inPassword);
-
-                IList<Doctor> foundDoctors = query.List<Doctor>();
-                
-                if (foundDoctors.Count == 0)
-                {
-                    return null;
-                }
-                else
-                {
-                    return foundDoctors[0];
-                }
-            }
-            */
             using (ISession session = NHibernateHelper.OpenSession())
             {
                 using (ITransaction transaction = session.BeginTransaction())
+                {
+                    IQuery query = session.CreateQuery(
+                            "FROM Doctor WHERE employee_id = :username AND password = :password");
+                    query.SetString("username", inId);
+                    query.SetString("password", inPassword);
+
+                    IList<Doctor> foundDoctors = query.List<Doctor>();
+
+                    if (foundDoctors.Count == 0)
                     {
-                        IQuery query = session.CreateQuery(
-                            "FROM Doctor");
-
-
-                        IList<Doctor> foundDoctors = query.List<Doctor>();
-
-                        if (foundDoctors.Count == 0)
-                        {
-                            return null;
-                        }
-                        else
-                        {
-                            return foundDoctors[0];
-                        }
+                        return null;
+                    }
+                    else
+                    {
+                        return foundDoctors[0];
+                    }
                 }
             }
         }
-
     }
 }
