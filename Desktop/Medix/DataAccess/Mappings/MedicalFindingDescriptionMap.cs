62647afec2248f4c01f00ce1b14c85869ba1dd78
Ivana Prebeg <ip48455@fer.hr>
2018-01-20 19:41:41 +0100

    bubolov mapiranja

---
+++ Desktop/Medix/DataAccess/Mappings/MedicalFindingDescriptionMap.cs
@@ -0,0 +1,22 @@
+﻿using FluentNHibernate.Mapping;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Mappings
+{
+    class MedicalFindingDescriptionMap : ClassMap<MedicalFindingDescription>
+    {
+        public MedicalFindingDescriptionMap()
+        {
+            Id(p => p.Id).GeneratedBy.Native();
+            References(p => p.Anamnesis);
+            References(p => p.Finding);
+            References(p => p.Status);
+            References(p => p.Therapy);
+        }
+    }
+}
