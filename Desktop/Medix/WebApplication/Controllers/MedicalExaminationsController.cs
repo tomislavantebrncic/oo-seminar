9b842cdf81ec54df119f3dde94edc1439bfaba16
Ivana Prebeg <ip48455@fer.hr>
2018-01-28 22:21:53 +0100

    rad na create examination

--- Desktop/Medix/WebApplication/Controllers/MedicalExaminationsController.cs
+++ Desktop/Medix/WebApplication/Controllers/MedicalExaminationsController.cs
@@ -11,11 +11,15 @@ namespace WebApplication.Controllers
     {
         private IMedicalExaminationService _exanimationService;
         private IPatientService _patientService;
+        private IDoctorService _doctorService;
         private IExaminationTypeService _typeService;
 
-        public MedicalExaminationsController(IMedicalExaminationService service)
+        public MedicalExaminationsController(IMedicalExaminationService service, IDoctorService doctorService, IExaminationTypeService typeService, IPatientService patientService)
         {
             _exanimationService = service;
+            _doctorService = doctorService;
+            _typeService = typeService;
+            _patientService = patientService;
         }
 
         // GET: MedicalExaminations
@@ -45,10 +49,9 @@ namespace WebApplication.Controllers
         }
 
         // GET: MedicalExaminations/Create
-        public ActionResult Create(int doctor_id)
+        public ActionResult Create(string employee_id)
         {
-            ViewBag.ExaminationType_id = new SelectList(_typeService.GetAll(), "Id", "Name");
-            ViewBag.Patient_id = new SelectList(_patientService.GetAll(), "Id", "FirstName");
+            ViewBag.ExaminationType = new SelectList(_typeService.GetAll());
             return View();
         }
 
@@ -57,78 +60,16 @@ namespace WebApplication.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,ExaminationDate,Examined,Doctor_id,Patient_id,ExaminationType_id,WaitingRoom_id")] MedicalExamination medicalExamination)
+        public ActionResult Create(MedicalExamination medicalExamination)
         {
             if (ModelState.IsValid)
             {
+                if  (_patientService.GetByOIB(medicalExamination.Patient.OIB) == null)
+                    ModelState.AddModelError("", "Neispravan OIB pacijenta.");
                 _exanimationService.Add(medicalExamination);
                 return RedirectToAction("Index");
             }
-
-            //ViewBag.Doctor_id = new SelectList(db.Doctors, "Id", "FirstName", medicalExamination.Doctor_id);
-            //ViewBag.ExaminationType_id = new SelectList(db.ExaminationTypes, "Id", "Name", medicalExamination.ExaminationType_id);
-            //ViewBag.WaitingRoom_id = new SelectList(db.WaitingRooms, "Id", "Name", medicalExamination.WaitingRoom_id);
-            //ViewBag.Patient_id = new SelectList(db.Patients, "Id", "FirstName", medicalExamination.Patient_id);
             return View(medicalExamination);
-        }
-
-        // GET: MedicalExaminations/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            MedicalExamination medicalExamination = _exanimationService.GetById(id.Value);
-            if (medicalExamination == null)
-            {
-                return HttpNotFound();
-            }
-            //ViewBag.Doctor_id = new SelectList(db.Doctors, "Id", "FirstName", medicalExamination.Doctor_id);
-            //ViewBag.ExaminationType_id = new SelectList(db.ExaminationTypes, "Id", "Name", medicalExamination.ExaminationType_id);
-            //ViewBag.WaitingRoom_id = new SelectList(db.WaitingRooms, "Id", "Name", medicalExamination.WaitingRoom_id);
-            //ViewBag.Patient_id = new SelectList(db.Patients, "Id", "FirstName", medicalExamination.Patient_id);
-            return View(medicalExamination);
-        }
-
-        // POST: MedicalExaminations/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,ExaminationDate,Examined,Doctor_id,Patient_id,ExaminationType_id,WaitingRoom_id")] MedicalExamination medicalExamination)
-        {
-            if (ModelState.IsValid)
-            {
-                //db.Entry(medicalExamination).State = EntityState.Modified;
-                //db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            //ViewBag.Doctor_id = new SelectList(db.Doctors, "Id", "FirstName", medicalExamination.Doctor_id);
-            //ViewBag.ExaminationType_id = new SelectList(db.ExaminationTypes, "Id", "Name", medicalExamination.ExaminationType_id);
-            //ViewBag.WaitingRoom_id = new SelectList(db.WaitingRooms, "Id", "Name", medicalExamination.WaitingRoom_id);
-            //ViewBag.Patient_id = new SelectList(db.Patients, "Id", "FirstName", medicalExamination.Patient_id);
-            return View(medicalExamination);
-        }
-
-        // POST: MedicalExaminations/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            //MedicalExamination medicalExamination = db.MedicalExaminations.Find(id);
-            //db.MedicalExaminations.Remove(medicalExamination);
-            //db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-               // db.Dispose();
-            }
-            base.Dispose(disposing);
         }
     }
 }
